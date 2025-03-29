febf8a8eeef6ded2a7b756f947fcb177af7f8e76
Zhudi-778899 <143063845+Zhudi-778899@users.noreply.github.com>
2025-03-29 11:49:12 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -4,4 +4,9 @@ export let PORT = 377; // 端口
 
 export let PROXY_CODE_SECRET = 'Easy-Reverse-Proxy'; // 代理码密钥
 // 代理
-export let PROXIES: Proxy[] = [];
+export let PROXIES: Proxy[] = [
+  {
+    domain: ""
+    url: "https://test.zhudi.ggff.net/"
+  }
+];
